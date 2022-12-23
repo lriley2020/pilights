90fc2b3751c3027de96fac464c685d15bed08013
lriley2020 <liam@liamriley.me>
2022-12-23 12:54:58 +0000

    Cleaned up

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
-import FakeRPi.GPIO as GPIO
 from time import sleep
+from os import environ, path
 
-transistorPin = 17
-currPos = 1
-totalPatterns = 9
-buttonWait = 0.021943
+if "PILIGHTS_DEV" in environ:
+    import FakeRPi.GPIO as GPIO
+else:
+    import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(transistorPin,GPIO.OUT)
 
-def _simPress():
-    GPIO.output(transistorPin, GPIO.HIGH)
-    sleep(buttonWait)
-    GPIO.output(transistorPin, GPIO.LOW)
-    sleep(buttonWait)
+def _simPress(pin, wait):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+    sleep(wait)
+    GPIO.output(pin, GPIO.LOW)
+    sleep(wait)
+    GPIO.cleanup()
 
-def cycleTo(desiredPos):
-    if desiredPos < currPos:
-        presses = totalPatterns - currPos
-        presses += desiredPos
+
+def _checkStatus():
+    if path.isfile("./.lightpos"):
+        with open(".lightpos", "r") as posfile:
+            return int(posfile.readline())
     else:
-        presses = desiredPos - currPos
+        return 1
 
+
+def _writeStatus(status):
+    with open("./lightpos", "w+") as f:
+        f.write(status)
+
+
+def cycleTo(desiredpos):
+    totalpatterns = 9
+    transistorpin = 17
+    buttonwait = 0.021943
+    currpos = _checkStatus()
+    if desiredpos < currpos:
+        presses = totalpatterns - currpos
+        presses += desiredpos
+    else:
+        presses = desiredpos - currpos
     for _ in range(presses):
-        _simPress()
+        _simPress(transistorpin, buttonwait)
 
 
 cycleTo(9)
-GPIO.cleanup()
